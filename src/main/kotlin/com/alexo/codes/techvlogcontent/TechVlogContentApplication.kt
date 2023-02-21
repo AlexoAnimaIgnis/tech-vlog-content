64c4df483c12e95035ba6b2d4b2a52f7eb5b0dd3
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2023-02-21 23:56:26 +0800

    initial commit

---
+++ src/main/kotlin/com/alexo/codes/techvlogcontent/TechVlogContentApplication.kt
@@ -0,0 +1,11 @@
+package com.alexo.codes.techvlogcontent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TechVlogContentApplication
+
+fun main(args: Array<String>) {
+    runApplication<TechVlogContentApplication>(*args)
+}
---
+++ src/test/kotlin/com/alexo/codes/techvlogcontent/TechVlogContentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alexo.codes.techvlogcontent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TechVlogContentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
